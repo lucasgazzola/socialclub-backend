41aa990097a664617c556f0c2ee4d56fc3692576
Gonza149 <elgonza149@gmail.com>
2026-07-04 18:51:44 -0300

    feat[01]: Agregado registro de usuario administrativo

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,22 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayNotEmpty, IsArray, IsEmail, IsString, MinLength } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsEmail, IsString, MinLength, Length, Matches, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateUsuarioDto {
+
+  @ApiProperty({ example: '123456789', description: 'DNI sin puntos o guiones' })
+  @IsString()
+  @Length(7, 8)
+  @Matches(/^\d+$/, {message: 'El DNI debe contener únicamente números'})
+  @IsNotEmpty()
+  dni: string;
+
   @ApiProperty({ example: 'coordinador@socialclub.local' })
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
-  @ApiProperty({ example: 'Cambiar123!', minLength: 8 })
+  @ApiProperty({ example: 'Cambiar123!', minLength: 8, description: 'Debe contener al menos una mayúscula, una minúscula, un número y un carácter especial.' })
   @IsString()
+  @IsNotEmpty()
   @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/, {message: 'La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial.' })
   password: string;
 
   @ApiProperty({ example: 'Ana' })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
+  @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s'-]+$/, { message: 'El nombre contiene caracteres inválidos'})
   nombre: string;
 
   @ApiProperty({ example: 'Pérez' })
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
+  @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s'-]+$/, { message: 'El apellido contiene caracteres inválidos' })
   apellido: string;
 
   @ApiProperty({ example: ['ADMIN'], description: 'Nombres de los roles a asignar' })
